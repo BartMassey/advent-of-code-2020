f6a9380b82b07ca220b3409f8d55b70d24a6294f
Bart Massey <bart.massey@gmail.com>
2020-12-01 22:57:16 -0800

    cleaned up day 01

--- day01/README.md
+++ day01/README.md
@@ -2,6 +2,20 @@
 Bart Massey
 
 ---
+Good warmup. I generalized my solution to handle sums of
+arbitrary length using **The Power Of Recursion.** I then
+examined `input.txt` and sped things up to negligible by:
+
+* Removing input numbers that add to more than 2020 when
+  added to the smallest input (most of them). This pretty
+  much takes care of Part 1.
+
+* Searching in descending order and skipping when the
+  partial sum is more than 2020. This pretty much takes care
+  of Part 2.
+
+Given these tricks, the whole thing could be done
+pencil-and-paper in a couple of minutes.
 
 Solution to
 [this problem](https://adventofcode.com/2020/day/1).
--- day01/soln.rs
+++ day01/soln.rs
@@ -7,34 +7,62 @@
 
 use aoc::*;
 
-fn find_sum(nums: &[u64], target: u64, count: usize, cur: u64) -> Option<Vec<u64>> {
+// Return the terms of the sum, if found.
+fn find_sum(
+    // Numbers to try.
+    nums: &[u64],
+    // Target sum.
+    target: u64,
+    // Number of terms remaining to find.
+    count: usize,
+    // Current partial sum.
+    cur: u64,
+) -> Option<Vec<u64>> {
+    // Prune if sum has gotten too large. Sort `nums` in
+    // descending order for best effect.
     if cur > target {
         return None;
     }
+
+    // Base case: no more numbers to sum.
     if count == 0 {
         if cur == target {
             return Some(Vec::new());
         }
         return None;
     }
+
+    // Recursive case: try to extend the sum by one more term.
     for (i, &n) in nums.iter().enumerate() {
-        if let Some(mut v) = find_sum(&nums[i + 1..], target, count - 1, cur + n) {
+        // Remove symmetry by searching only ahead in `nums`.
+        let s = find_sum(&nums[i + 1..], target, count - 1, cur + n);
+        if let Some(mut v) = s {
             v.push(n);
             return Some(v);
         }
     }
+
+    // No solution on this branch.
     None
 }
 
 pub fn main() {
-    let mut nums: Vec<u64> = input_lines().map(|l| l.parse().unwrap()).collect();
+    // Collect input.
+    let mut nums: Vec<u64> =
+        input_lines().map(|l| l.parse().unwrap()).collect();
+
+    // Remove "too large" inputs.
     nums.sort_by_key(|&n| std::cmp::Reverse(n));
     let smallest = *nums.last().unwrap();
     nums.retain(move |&n| n != smallest && n + smallest <= 2020);
+
+    // Find the sum.
     let soln = match get_part() {
         Part1 => find_sum(&nums, 2020, 2, 0).unwrap(),
         Part2 => find_sum(&nums, 2020, 3, 0).unwrap(),
     };
-    let p: u64 = soln.iter().cloned().product();
+
+    // Show the product.
+    let p: u64 = soln.into_iter().product();
     println!("{}", p);
 }
