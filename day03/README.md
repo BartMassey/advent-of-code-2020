148ec859c37206fdca4a09e11a5258ad494499a4
Bart Massey <bart.massey@gmail.com>
2020-12-02 22:04:08 -0800

    did day 03

---
+++ day03/README.md
@@ -0,0 +1,25 @@
+# Advent of Code 2020: Day 3
+Bart Massey
+
+---
+
+Simple little exercise. Messed up badly writing the loop the
+first time, which was dumb.
+
+Clippy's warning about `Vec` parameters is super-annoying
+for named types.
+
+---
+
+Solution to [this problem](https://adventofcode.com/2020/day/3).
+
+Save your problem input to `input.txt` and run with
+
+    cargo run --release 1 <input.txt
+    cargo run --release 2 <input.txt
+
+---
+
+This program is licensed under the "MIT License".
+Please see the file LICENSE in this distribution
+for license terms.
---
+++ day03/soln.rs
@@ -0,0 +1,58 @@
+// This program is licensed under the "MIT License".
+// Please see the file LICENSE in this distribution
+// for license terms.
+
+//! Advent of Code Day 3.  
+//! Bart Massey 2020
+
+use aoc::*;
+
+type Map = Vec<Vec<bool>>;
+
+fn read_map() -> Map {
+    let mut map = Vec::new();
+    for line in input_lines() {
+        let mut row = Vec::new();
+        for ch in line.chars() {
+            match ch {
+                '#' => row.push(true),
+                '.' => row.push(false),
+                '\n' => (),
+                _ => panic!("unexpected char"),
+            }
+        }
+        map.push(row);
+    }
+    map
+}
+
+#[allow(clippy::ptr_arg)]
+fn count_trees(map: &Map, right: usize, down: usize) -> usize {
+    let height = map.len();
+    let width = map[0].len();
+    let mut col = 0;
+    let mut ntrees = 0;
+    for row in (0..height).step_by(down) {
+        ntrees += usize::from(map[row][col]);
+        col = (col + right) % width;
+    }
+    ntrees
+}
+
+pub fn main() {
+    let map = read_map();
+    match get_part() {
+        Part1 => {
+            let ntrees = count_trees(&map, 3, 1);
+            println!("{}", ntrees);
+        }
+        Part2 => {
+            let slopes = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+            let tree_mult: usize = slopes
+                .into_iter()
+                .map(|(down, right)| count_trees(&map, down, right))
+                .product();
+            println!("{}", tree_mult);
+        }
+    }
+}
