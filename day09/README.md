a4c6a5df8d68e6dda6c5227de8de7d02657769dd
Bart Massey <bart.massey@gmail.com>
2020-12-08 22:51:39 -0800

    finished day 09 (again, see readme)

---
+++ day09/README.md
@@ -0,0 +1,45 @@
+# Advent of Code 2020: Day 9
+Bart Massey
+
+---
+
+Realistically my difficulties in correctly reading the
+problem description were the biggest time-sink on this one,
+in spite of my trying to be careful.
+
+Looked like Part 2 was finally the dreaded dynamic
+programming problem. However, I found a sliding-window
+algorithm that looked to be both faster and simpler. Tried
+my algorithm, didn't seem to work, convinced myself it was
+wrong. Went on to spend a bunch of time on a dynamic
+programming solution that I knew would work. Solved Part 2.
+
+After I'd debugged everything, I went back and
+re-implemented my original algorithm. Worked fine, much
+simpler, 10× faster.
+
+See the branch `day09-dynamic-programming` in this repo for
+the previous solution. Sigh.
+
+I really wish the test instance wouldn't have
+different parameters than the input instance: it is easy to
+forget to switch between the two. Ideally, the instance
+would just start with the window length.
+
+This is *not* a decryption problem. I don't know why it is
+labeled as one.
+
+---
+
+Solution to [this problem](https://adventofcode.com/2020/day/9).
+
+Save your problem input to `input.txt` and run with
+
+    cargo run --release 1 <input.txt
+    cargo run --release 2 <input.txt
+
+---
+
+This program is licensed under the "MIT License".
+Please see the file LICENSE in this distribution
+for license terms.
---
+++ day09/soln.rs
@@ -0,0 +1,75 @@
+// This program is licensed under the "MIT License".
+// Please see the file LICENSE in this distribution
+// for license terms.
+
+//! Advent of Code Day 9.  
+//! Bart Massey 2020
+
+use std::cmp::Ordering;
+
+use aoc::*;
+
+fn has_sum(seq: &[u64], v: u64) -> bool {
+    for i in seq {
+        for j in seq {
+            if j == i {
+                continue;
+            }
+            if i + j == v {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn find_unmatched(seq: &[u64], pre_len: usize) -> u64 {
+    let nseq = seq.len();
+    for i in pre_len..nseq {
+        let subseq = &seq[i - pre_len..i];
+        if !has_sum(subseq, seq[i]) {
+            return seq[i];
+        }
+    }
+    panic!("no unmatched");
+}
+
+fn find_subseq(seq: &[u64], invalid: u64) -> (usize, usize) {
+    let mut sum = seq[0];
+    let mut i = 0;
+    let mut j = 0;
+    let nseq = seq.len();
+    while i < nseq {
+        match u64::cmp(&sum, &invalid) {
+            Ordering::Less => {
+                j += 1;
+                sum += seq[j];
+            }
+            Ordering::Greater => {
+                sum -= seq[i];
+                i += 1;
+            }
+            Ordering::Equal => {
+                return (i, j);
+            }
+        }
+    }
+    panic!("no subseq");
+}
+
+fn main() {
+    let seq: Vec<u64> = input_lines().map(|l| l.parse().unwrap()).collect();
+    let unmatched = find_unmatched(&seq, 25);
+    match get_part() {
+        Part1 => {
+            println!("{}", unmatched);
+        },
+        Part2 => {
+            let (i, j) = find_subseq(&seq, unmatched);
+            let subseq = &seq[i..=j];
+            let smallest = subseq.iter().cloned().min().unwrap();
+            let largest = subseq.iter().cloned().max().unwrap();
+            println!("{}", smallest + largest);
+        },
+    }
+}
